ac15d65762908eee5595ad6fa0fc94977fb4ce2d
Amy Spears <amspears007@gmail.com>
2023-01-03 10:11:22 -0700

    Add 'exhibits' test to Museum class

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -14,6 +14,20 @@ RSpec.describe Museum do
   it 'starts with no exhibits' do
     dmns = Museum.new("Denver Museum of Nature and Science")
 
-    expect(dmns.exhibits).to eq []
+    expect(dmns.exhibits).to eq([])
+  end
+
+  it 'can have exhibits' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    expect(dmns.exhibits).to eq([dmns.exhibits, dead_sea_scrolls, dead_sea_scrolls ])
+
   end
 end
