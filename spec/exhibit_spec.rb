bab40423c1d5ae55fe58c34f68339b883eeefda7
Amy Spears <amspears007@gmail.com>
2023-01-03 09:25:50 -0700

    Add 1st passing test in Exhibit class

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -5,5 +5,10 @@ RSpec.describe Exhibit do
 
   it 'exists' do
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    expect(exhibit).to be_a(Exhibit)
+    expect(exhibit.name).to eq("Gems and Minerals")
+    expect(exhibit.cost).to eq(0)
+  end
   
 end
