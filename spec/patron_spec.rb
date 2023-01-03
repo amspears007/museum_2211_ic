5cbba8a0841d252ee8006c4728ae6ee1ede4ada5
Amy Spears <amspears007@gmail.com>
2023-01-03 09:39:17 -0700

    Add 2nd test in Patron class

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -2,8 +2,17 @@ require 'rspec'
 require './lib/patron'
 
 RSpec.describe Patron do
-  it 'exists' do
+  it 'exists and has attributes' do
 		patron_1 = Patron.new("Bob", 20)
+
 		expect(patron_1).to be_a(Patron)
+		expect(patron_1.name).to eq("Bob")
+		expect(patron_1.spending_money).to eq(20)
   end
+
+	it 'it begins with no interests' do
+		patron_1 = Patron.new("Bob", 20)
+		
+		expect(patron_1.interests).to eq([])	
+	end
 end
